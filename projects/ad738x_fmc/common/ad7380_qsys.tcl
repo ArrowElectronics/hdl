c18ff037afd57c830a9d42bf75ef917d5778fa6f
snehal buche <snehal.buche@einfochips.com>
2021-09-08 21:29:04 +0530

    added ad738x project with modified spi_engine_execution & util_upscale

---
+++ projects/ad738x_fmc/common/ad7380_qsys.tcl
@@ -0,0 +1,23 @@
+set_instance_parameter_value spi_engine_execution_0 {DATA_WIDTH} {18}
+set_instance_parameter_value spi_engine_execution_0 {NUM_OF_SDI} {2}
+#set_instance_parameter_value spi_engine_execution_0 {ONE_BIT_SHIFT} {0}
+
+set_instance_parameter_value spi_engine_interconnect_0 {DATA_WIDTH} {18}
+set_instance_parameter_value spi_engine_interconnect_0 {NUM_OF_SDI} {2}
+
+set_instance_parameter_value upscale_converter_0 {DATA_WIDTH} {18}
+set_instance_parameter_value upscale_converter_0 {UDATA_WIDTH} {32}
+set_instance_parameter_value upscale_converter_0 {NUM_OF_CHANNELS} {2}
+
+set_instance_parameter_value spi_engine_offload_0 {DATA_WIDTH} {18}
+set_instance_parameter_value spi_engine_offload_0 {NUM_OF_SDI} {2}
+
+set_instance_parameter_value axi_spi_engine_0 {DATA_WIDTH} {18}
+set_instance_parameter_value axi_spi_engine_0 {NUM_OF_SDI} {2}
+
+set_instance_parameter_value spi_dmac_0 {DMA_DATA_WIDTH_SRC} {64}
+
+set_instance_parameter_value trigger_gen_0 {PULSE_PERIOD} {218}
+
+
+
